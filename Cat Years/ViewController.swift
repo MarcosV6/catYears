a06f12a8f2b6e7aab4bcbbb8576ac6d08d9864f1
marcos vallejo <thisismarcos@gmail.com>
2016-06-08 12:19:09 -0400

    commit

---
+++ Cat Years/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  Cat Years
+//
+//  Created by marcos vallejo on 2/28/16.
+//  Copyright © 2016 SpiderApps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var catAgeTextField: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    @IBAction func findAge(sender: AnyObject) {
+        
+        var catAge = Int(catAgeTextField.text!)!
+        
+        catAge = catAge * 7
+        
+        resultLabel.text = "your cat is \(catAge)  in cat years"
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
